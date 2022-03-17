1d7524235909bbe9dff4d01750d7b61ea6b1d4d1
michaeljoy255 <kodiakchaos@gmail.com>
2022-03-16 23:31:29 -0400

    Figured out how to mock LocalDatabase

--- src/services/__tests__/LocalDatabase.test.ts
+++ src/services/__tests__/LocalDatabase.test.ts
@@ -1,64 +1,73 @@
 import { describe, test, expect, beforeEach, vi } from 'vitest'
-import type { IUser } from '../../models/User'
-import type { IExample } from '../../models/Example'
 import { LocalDatabase } from '../LocalDatabase'
-
-class MockUser implements IUser {
-  id: string
-  createdDate: string
-  name: string
-  attributes: object
-
-  constructor() {
-    this.id = 'USER-1234'
-    this.createdDate = new Date('2022/1/1').toISOString()
-    this.name = 'Test User'
-    this.attributes = { attrs: 123 }
-  }
-}
-
-class MockExample implements IExample {
-  id: string
-  createdDate: string
-  data: object
-
-  constructor() {
-    this.id = 'EXAMPLE-1234'
-    this.createdDate = new Date('2022/2/2').toISOString()
-    this.data = { datas: 123 }
-  }
-}
 
 describe('LocalDatabase', () => {
   let db: any
+  const toArrayMock = vi.fn()
+  const firstMock = vi.fn()
+  const lastMock = vi.fn()
+  const tableMock = vi.fn(() => ({ toArray: toArrayMock }))
+  const equalsIgnoreCaseMock = vi.fn(() => ({ first: firstMock }))
+  const whereMock = vi.fn(() => ({ equalsIgnoreCase: equalsIgnoreCaseMock }))
+  const orderByMock = vi.fn(() => ({ first: firstMock, last: lastMock }))
 
   beforeEach(() => {
-    vi.clearAllMocks()
     db = new LocalDatabase()
-    db.users.add = vi.fn()
-    db.examples.add = vi.fn()
+    db.users = {
+      add: vi.fn(),
+      where: whereMock,
+    }
+    db.examples = {
+      add: vi.fn(),
+      orderBy: orderByMock,
+    }
+    db.table = tableMock
+    vi.clearAllMocks()
   })
 
-  test('should call add from addUser with provided parameter', () => {
-    db.addUser(new MockUser())
-    expect(db.users.add).toHaveBeenCalledWith(
-      Object.assign(new MockUser(), {
-        attributes: { attrs: 123 },
-        createdDate: '2022-01-01T05:00:00.000Z',
-        id: 'USER-1234',
-        name: 'Test User',
-      })
-    )
+  test('should call Dexie methods from addUser with provided parameter', () => {
+    const mockUser = {
+      attributes: { attrs: 123 },
+      createdDate: '2022-01-01T05:00:00.000Z',
+      id: 'USER-1234',
+      name: 'Test User',
+    }
+    db.addUser(mockUser)
+    expect(db.users.add).toHaveBeenCalledWith(mockUser)
   })
 
-  test('should call add from addExample with provided parameter', () => {
-    db.addUser(new MockExample())
-    expect(db.users.add).toHaveBeenCalledWith(
-      Object.assign(new MockExample(), {
-        data: { datas: 123 },
-        createdDate: '2022-02-02T05:00:00.000Z',
-        id: 'EXAMPLE-1234',
-      })
-    )
+  test('should call Dexie methods from addExample with provided parameter', () => {
+    const mockExample = {
+      data: { datas: 123 },
+      createdDate: '2022-02-02T05:00:00.000Z',
+      id: 'EXAMPLE-1234',
+    }
+    db.addExample(mockExample)
+    expect(db.examples.add).toHaveBeenCalledWith(mockExample)
+  })
+
+  test('should call Dexie methods from getAllFromStore with provided parameter', () => {
+    db.getAllFromStore('test')
+    expect(db.table).toHaveBeenCalledWith('test')
+    expect(toArrayMock).toHaveBeenCalled()
+  })
+
+  test('should call Dexie methods from getUserByName with provided parameter', () => {
+    db.getUserByName('Test User')
+    expect(db.users.where).toHaveBeenCalledWith('name')
+    expect(equalsIgnoreCaseMock).toHaveBeenCalledWith('Test User')
+    expect(firstMock).toHaveBeenCalled()
+  })
+
+  test('should call Dexie methods from getNewestExample with provided parameter', () => {
+    db.getNewestExample()
+    expect(db.examples.orderBy).toHaveBeenCalledWith('createdDate')
+    expect(lastMock).toHaveBeenCalled()
+  })
+
+  test('should call Dexie methods from getOldestExample with provided parameter', () => {
+    db.getOldestExample()
+    expect(db.examples.orderBy).toHaveBeenCalledWith('createdDate')
+    expect(firstMock).toHaveBeenCalled()
   })
 })
