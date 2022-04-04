77a0bb512d5e3e98d29bdf378d1d3f97abbb97d6
michaeljoy255 <kodiakchaos@gmail.com>
2022-04-04 19:53:54 -0400

    ui store tests

---
+++ src/stores/__tests__/ui.test.ts
@@ -0,0 +1,32 @@
+import { describe, test, expect, beforeEach } from 'vitest'
+import { setActivePinia, createPinia } from 'pinia'
+import { useUIStore } from '../ui'
+
+describe('useUIStore', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  test('toggleDrawer toggles the drawer boolean', () => {
+    const ui = useUIStore()
+    ui.drawer = false
+    ui.toggleDrawer()
+    expect(ui.drawer).toBe(true)
+    ui.toggleDrawer()
+    expect(ui.drawer).toBe(false)
+  })
+
+  test('openDrawer sets the drawer boolean to true', () => {
+    const ui = useUIStore()
+    ui.drawer = false
+    ui.openDrawer()
+    expect(ui.drawer).toBe(true)
+  })
+
+  test('closeDrawer sets the drawer boolean to false', () => {
+    const ui = useUIStore()
+    ui.drawer = true
+    ui.closeDrawer()
+    expect(ui.drawer).toBe(false)
+  })
+})
