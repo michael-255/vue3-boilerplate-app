35266d7515839feb8a7654d2a3e76b9cad66ebba
michaeljoy255 <kodiakchaos@gmail.com>
2022-04-05 20:04:11 -0400

    first pass at MenuLayout

--- src/layouts/MenuLayout.vue
+++ src/layouts/MenuLayout.vue
@@ -1,14 +1,42 @@
 <script setup lang="ts">
+import { QBtn, QLayout, QHeader, QToolbar, QToolbarTitle, QDrawer, QPageContainer } from 'quasar'
 import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
 </script>
 
 <template>
-  <h2>Menu Layout</h2>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/dashboard">Dashboard</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-    <RouterLink to="/test123">NotFound</RouterLink>
-  </nav>
-  <slot />
+  <QLayout elevated view="hHh lpR lFf">
+    <QHeader bordered class="bg-primary text-white">
+      <QToolbar>
+        <QBtn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <QToolbarTitle> MenuLayout </QToolbarTitle>
+      </QToolbar>
+    </QHeader>
+
+    <QDrawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <div>
+        <RouterLink to="/">Home</RouterLink>
+      </div>
+      <div>
+        <RouterLink to="/dashboard">Dashboard</RouterLink>
+      </div>
+      <div>
+        <RouterLink to="/about">About</RouterLink>
+      </div>
+      <div>
+        <RouterLink to="/test123">NotFound</RouterLink>
+      </div>
+    </QDrawer>
+
+    <QPageContainer>
+      <router-view />
+    </QPageContainer>
+  </QLayout>
 </template>
