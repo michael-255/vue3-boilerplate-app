009ac4af8a9ebf92051e97c7e6032b9e539c0142
michaeljoy255 <kodiakchaos@gmail.com>
2022-04-06 20:14:11 -0400

    cleaning out some files

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -9,19 +9,3 @@ console.log(route.meta)
 <template>
   <h1>About View</h1>
 </template>
-
-<!-- <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style> -->
