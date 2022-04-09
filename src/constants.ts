824b2ffac95d4d5269acace8cbe4fb5443fa2f03
michaeljoy255 <kodiakchaos@gmail.com>
2022-04-09 18:57:37 -0400

    cleaned up constants into TS enums

--- src/constants.ts
+++ src/constants.ts
@@ -1,20 +1,17 @@
 export const DEBUG = Object.freeze(true)
 
-const view = 'View'
-const layout = 'Layout'
+export enum Paths {
+  DASHBOARD = '/dashboard',
+  ABOUT = '/about',
+}
 
-export const Paths = Object.freeze({
-  DASHBOARD: '/dashboard',
-  ABOUT: '/about',
-})
+export enum Layouts {
+  DEFAULT = 'DefaultLayout',
+  MENU = 'MenuLayout',
+}
 
-export const Layouts = Object.freeze({
-  DEFAULT: 'Default' + layout,
-  MENU: 'Menu' + layout,
-})
-
-export const Views = Object.freeze({
-  DASHBOARD: 'Dashboard' + view,
-  ABOUT: 'About' + view,
-  NOTFOUND: 'NotFound' + view,
-})
+export enum Views {
+  DASHBOARD = 'DashboardView',
+  ABOUT = 'AboutView',
+  NOTFOUND = 'NotFoundView',
+}
